5fd52e730bf015fc0d10820a9051f969b4cc2744
Keily <kbtelles@scl.edu.gt>
2023-05-15 15:56:59 -0600

    Calculadora realizada.

---
+++ js/main.js
@@ -0,0 +1,54 @@
+render_keys();
+
+let results = document.querySelector(".resultado");
+
+function add_key(){
+    let val = this.innerHTML;
+    results = document.querySelector(".resultado");
+    results.value = results.value + val;
+}
+
+function reset_operation(){
+    results = document.querySelector(".resultado");
+    results.value = "";
+}
+
+function reset_all(){
+    let results2 = document.querySelector(".resultado");
+    results2.value = "0";
+    document.querySelector(".record").innerHTML = "";
+}
+function delete_letter(){
+    var content = results.value;
+    var nuevoValor = content.slice(0, -1);
+    results.value = nuevoValor;
+}
+
+function calculate(){
+    operation = document.querySelector(".resultado");
+    let r = eval(operation.value);
+    set_record(operation.value,r);
+    operation.value = r;
+}
+
+// Evento de todas las teclas
+let keys_dom = document.querySelectorAll(".key_item");
+
+keys_dom.forEach((keys_content) => {
+    let content = keys_content.innerHTML;
+
+    if (content == "C") {
+        keys_content.addEventListener("click", reset_operation);
+    } else if (content == "=") {
+        keys_content.addEventListener("click", calculate);
+    } else if (content == "AC") {
+        keys_content.addEventListener("click", reset_all);
+    } else if (content == '<img src="./img/delete.png" class="delete">') {
+        keys_content.addEventListener("click", delete_letter);
+    } else {
+        keys_content.addEventListener("click", add_key);
+    }
+});
+
+
+
